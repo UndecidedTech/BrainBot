f2b33bba05dcf06655afdeb0e2887b951480d928
twigie <spartans4348@yahoo.com>
2020-02-15 10:42:06 -0500

    Integrated the new music player into out command parsing if statement

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ bot.on("ready", () => {
 bot.on("reconnecting", () => {
   console.log("BrainBot is attempting to Reconnect")
 })
-bot.on('message', async message => {
+/*bot.on('message', async message => {
   if (message.content === '&play') {
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
@@ -60,9 +60,9 @@ bot.on('message', async message => {
       message.reply('You need to join a voice channel first')
     }
   }
-})
+})*/
 
-/*bot.on("message",  async (msg) => {
+bot.on("message",  async (msg) => {
     const parsed_msg = parser.parse(msg, prefix);
     if (parsed_msg.command === "register") {
         msg.channel.send(`SHUT UP ${parsed_msg.arguments[0]}`);
@@ -71,7 +71,8 @@ bot.on('message', async message => {
         const channel = msg.member.voiceChannel;
         summonBot(channel)
     }else if (parsed_msg.command === "play") {
-        
+        const channel = msg.member.voice.channel;
+        playMusic(channel)
     }
 
     
@@ -81,9 +82,24 @@ function summonBot (channel) {
     .then(connection => console.log("Connected to Voice Channel"))
     .catch(console.error)
 }
- /*function playMusic (channel) {
+function playMusic (channel) {
     channel.join().then(connection => {
-    let dispatcher =  connection.play('/home/twigie/Projects/BrainBot/Assets/dr.mp3', {volume: 1 })
+    let dispatcher =  connection.play(ytdl("https://www.youtube.com/watch?v=aYekOTLmOLs", {format: 'audioonly'}))
+        .on('end', reason => {
+            if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+            else console.log("Reason: ", reason);
+         })
+        .on('error', error => console.error(error));
+    dispatcher.setVolumeLogarithmic(2 / 5);
+
+
+    dispatcher.on('start', () => {
+        console.log('Audio Stream is Now Playing!');
+    });
+
+    dispatcher.on('finish', () => {
+        console.log('Audio Stream has Finished Playing!');
+    });
     
     });
-}*/
+}
